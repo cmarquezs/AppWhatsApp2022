60712b2a7235128a91a2db710c7c2105b30685fe
Cristian Marquez <Cristian Marquez@DESKTOP-22KBONV>
2022-03-01 12:11:00 -0500

    Agregar archivos de proyecto.

---
+++ AppWhatsApp/AppWhatsApp/MainPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using AppWhatsApp.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppWhatsApp
+{
+    public partial class MainPage : TabbedPage
+    {
+        public List<Contactos> ListaChats { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Contactos p1 = new Contactos
+            {
+                Nombre = "Cristian Marquez",
+                Chat = "Hola, hace hambre!!!",
+                Img = "foto.png",
+                Horachat = "01:52 p. m.",
+                Horallamada = "Ayer 5:18 p. m.",
+                EstadoContacto = "Hoy 11:55 a.m",
+                Call = "videocall.png"
+            };
+
+            Contactos p2 = new Contactos
+            {
+                Nombre = "Nataly Betancur",
+                Chat = "Hola, ¿cómo estás?",
+                Img = "girl.jpg",
+                Horachat = "11:19 a. m.",
+                Horallamada = "Ayer 9:23 a. m.",
+                EstadoContacto = "Hoy 12:55 a. m.",
+                Call = "voicecall.png"
+            };
+
+
+
+            ListaChats = new List<Contactos>();
+            ListaChats.Add(p1);
+            ListaChats.Add(p2);
+            ListaChats.Add(p1);
+            ListaChats.Add(p2);
+            ListaChats.Add(p1);
+            ListaChats.Add(p2);
+            ListaChats.Add(p1);
+            ListaChats.Add(p2);
+            ListaChats.Add(p1);
+            ListaChats.Add(p2);
+            ListaChats.Add(p1);
+            ListaChats.Add(p2); 
+            ListaChats.Add(p1);
+            ListaChats.Add(p2);
+            ListaChats.Add(p1);
+            ListaChats.Add(p2);
+            ListaChats.Add(p1);
+            ListaChats.Add(p2); 
+            ListaChats.Add(p1);
+            ListaChats.Add(p2);
+            ListaChats.Add(p1);
+            ListaChats.Add(p2);
+            ListaChats.Add(p1);
+            ListaChats.Add(p2);
+
+
+            this.BindingContext = this;
+
+
+        }
+    }
+}
---
+++ AppWhatsApp/AppWhatsApp/Modelos/Contactos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppWhatsApp.Modelos
+{
+    public class Contactos
+    {
+        public string Nombre { get; set; }
+        public string Chat { get; set; }
+        public string Img { get; set; }
+        public string Horachat { get; set; }
+        public string Horallamada { get; set; }
+        public string EstadoContacto { get; set; }
+        public string Call { get; set; }
+
+    }
+}
